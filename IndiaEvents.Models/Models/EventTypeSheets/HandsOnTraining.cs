f8378378b7a76ef03b8a21e7a2f21c5df98c9fae
swalame123 <srikanth.n@congruentglobal.com>
2024-03-25 15:55:18 +0530

    VenueSelectionChecklistMaster

---
+++ IndiaEvents.Models/Models/EventTypeSheets/HandsOnTraining.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndiaEvents.Models.Models.EventTypeSheets
+{
+    public class HandsOnTraining
+    {
+
+    }
+}
