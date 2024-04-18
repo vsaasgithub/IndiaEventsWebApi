a21c11efa064309cdfea24468f1bb8c828117fde
swalame123 <srikanth.n@congruentglobal.com>
2024-04-18 14:15:47 +0530

    Added venuename

--- IndiaEvents.Models/Models/Draft/UpdateDataForClassI.cs
+++ IndiaEvents.Models/Models/Draft/UpdateDataForClassI.cs
@@ -9,18 +9,20 @@ namespace IndiaEvents.Models.Models.Draft
 {
     public class UpdateDataForClassI
     {
-       
+
         public EventDetails EventDetails { get; set; }
         public List<BrandSelection> BrandSelection { get; set; }
         public List<PanelSelection> PanelSelection { get; set; }
         public List<SlideKitSelection> SlideKitSelection { get; set; }
         public List<InviteeSelection> InviteeSelection { get; set; }
         public List<ExpenseSelection> ExpenseSelection { get; set; }
-        public Files files { get; set; }
+        public string IsDeviationUpload { get; set; }
+        public string DeviationFiles { get; set; }
     }
 
     public class BrandSelection
     {
+        public string Id { get; set; }
         public string brandName { get; set; }
         public int percentageAllocation { get; set; }
         public string projectId { get; set; }
@@ -28,44 +30,62 @@ namespace IndiaEvents.Models.Models.Draft
 
     public class EventDetails
     {
+        public string Id { get; set; }
         public string EventTopic { get; set; }
         public string StartTime { get; set; }
         public string EndTime { get; set; }
         public string VenueName { get; set; }
         public string State { get; set; }
         public string City { get; set; }
+        public string MeetingType { get; set; }
+        public string Brands { get; set; }
+        public string Panelists { get; set; }
+        public string SlideKits { get; set; }
+        public string Invitees { get; set; }
+        public string MIPLInvitees { get; set; }
+        public string Expenses { get; set; }
+        public int? TotalExpenseBTC { get; set; }
+        public int? TotalExpenseBTE { get; set; }
+        public int? TotalHonorariumAmount { get; set; }
+        public int? TotalTravelAccommodationAmount { get; set; }
+        public int? TotalAccomodationAmount { get; set; }
+        public int? TotalBudget { get; set; }
+        public int? TotalLocalConveyance { get; set; }
+        public int? TotalTravelAmount { get; set; }
+        public int? TotalExpense { get; set; }
+
+        public string IsFilesUpload { get; set; }
+        public List<string> Files { get; set; }
+
     }
 
     public class ExpenseSelection
     {
-        public string ExpenseName { get; set; }
+        public string Id { get; set; }
+        public string Expense { get; set; }
         public string ExpenseType { get; set; }
         public int ExpenseAmountIncludingTax { get; set; }
         public int ExpenseAmountExcludingTax { get; set; }
         public string FoodAndBeveragedDeviationUrl { get; set; }
     }
 
-    public class Files
-    {
-        public string AgendaFileUrl { get; set; }
-        public string InvitationFileUrl { get; set; }
-    }
-
     public class InviteeSelection
     {
+        public string Id { get; set; }
         public string InviteeFrom { get; set; }
         public string Name { get; set; }
         public string MisCode { get; set; }
         public string EmployeeCode { get; set; }
-        public string isLocalConveyance { get; set; }
-        public string localConveyanceType { get; set; }
-        public string speciality { get; set; }
-        public int localConveyanceAmountIncludingTax { get; set; }
-        public int localConveyanceAmountExcludingTax { get; set; }
+        public string IsLocalConveyance { get; set; }
+        public string LocalConveyanceType { get; set; }
+        public string Speciality { get; set; }
+        public int LocalConveyanceAmountIncludingTax { get; set; }
+        public int LocalConveyanceAmountExcludingTax { get; set; }
     }
 
     public class PanelSelection
     {
+        public string Id { get; set; }
         public string SpeakerCode { get; set; }
         public string TrainerCode { get; set; }
         public string Speciality { get; set; }
@@ -73,9 +93,9 @@ namespace IndiaEvents.Models.Models.Draft
         public string Qualification { get; set; }
         public string Country { get; set; }
         public string Rationale { get; set; }
-        public string nocFileUrl { get; set; }
-        public string fcpaIssueDate { get; set; }
-        public string fcpaFile { get; set; }
+        public string NocFileUrl { get; set; }
+        public string FcpaIssueDate { get; set; }
+
         public int PresentationDuration { get; set; }
         public int PanelSessionPreperationDuration { get; set; }
         public int PanelDisscussionDuration { get; set; }
@@ -99,8 +119,8 @@ namespace IndiaEvents.Models.Models.Draft
         public int AccomdationIncludingTax { get; set; }
         public int AccomdationExcludingTax { get; set; }
         public string AccomodationBtcorBte { get; set; }
-        public string aggregateDeviationFileUrl { get; set; }
-        public string honorariumDeviationFileUrl { get; set; }
+        public string AggregateDeviationFileUrl { get; set; }
+        public string HonorariumDeviationFileUrl { get; set; }
         public string PanCardName { get; set; }
         public string BankAccountNumber { get; set; }
         public string IFSCCode { get; set; }
@@ -109,43 +129,22 @@ namespace IndiaEvents.Models.Models.Draft
         public string OtherCurrencyType { get; set; }
         public string BeneficiaryName { get; set; }
         public string PanNumber { get; set; }
-        public string isGlobalFMVCheck { get; set; }
+        public string IsGlobalFMVCheck { get; set; }
         public string SwiftCode { get; set; }
-        public string PanCardFileUrl { get; set; }
-        public string ChequeFileUrl { get; set; }
-        public string TaxResidentialFileUrl { get; set; }
-    }
-
-    public class PreEventCheck
-    {
-        [JsonProperty("45DaysDeviationFileUrl")]
-        public string _45DaysDeviationFileUrl { get; set; }
-        public string eventDate { get; set; }
-
-        [JsonProperty("5DaysDeviationFileUrl")]
-        public string _5DaysDeviationFileUrl { get; set; }
-    }
-
-    public class Root
-    {
-        public PreEventCheck preEventCheck { get; set; }
-        public EventDetails eventDetails { get; set; }
-        public List<BrandSelection> brandSelection { get; set; }
-        public List<PanelSelection> panelSelection { get; set; }
-        public List<SlideKitSelection> slideKitSelection { get; set; }
-        public List<InviteeSelection> inviteeSelection { get; set; }
-        public List<ExpenseSelection> expenseSelection { get; set; }
-        public Files files { get; set; }
+        public string IsFilesUpload { get; set; }
+        public List<string> Files { get; set; }
     }
 
     public class SlideKitSelection
     {
-        public string hcpName { get; set; }
-        public string misCode { get; set; }
-        public string hcpType { get; set; }
-        public string slideKitType { get; set; }
-        public string slideKitOption { get; set; }
-        public string slideKitFileUrl { get; set; }
+        public string Id { get; set; }
+        public string HcpName { get; set; }
+        public string MisCode { get; set; }
+        public string HcpType { get; set; }
+        public string SlideKitType { get; set; }
+        public string SlideKitOption { get; set; }
+        public string IsFilesUpload { get; set; }
+        public List<string> Files { get; set; }
     }
 
 
