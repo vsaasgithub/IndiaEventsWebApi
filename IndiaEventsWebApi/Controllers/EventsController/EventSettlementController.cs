9b87991eb6c2be7682d7069c98f35530fe86f673
swalame123 <srikanth.n@congruentglobal.com>
2024-05-07 19:11:02 +0530

    BudgetAmount,ActualAmount updated in event settlement sheet

--- IndiaEventsWebApi/Controllers/EventsController/EventSettlementController.cs
+++ IndiaEventsWebApi/Controllers/EventsController/EventSettlementController.cs
@@ -283,7 +283,7 @@ namespace IndiaEventsWebApi.Controllers
                 int addedExpencesNo = 1;
                 foreach (var formdata in formData.expenseSheets)
                 {
-                    string rowData = $"{addedExpencesNo}. {formdata.Expense} | Budget Amount: {formdata.AmountExcludingTax}| Actual Amount: {formdata.Amount} | {formdata.BtcorBte}";
+                    string rowData = $"{addedExpencesNo}. {formdata.Expense} | Budget Amount: {formdata.BudgetAmount}| Actual Amount: {formdata.ActualAmount} | {formdata.BtcorBte}";
                     addedExpences.AppendLine(rowData);
                     addedExpencesNo++;
                 }
