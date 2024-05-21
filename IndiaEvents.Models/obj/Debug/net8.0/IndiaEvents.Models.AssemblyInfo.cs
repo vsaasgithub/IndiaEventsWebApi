9345ecaa0f5784721f2f7f1de9b10a07a69de0b1
swalame123 <srikanth.n@congruentglobal.com>
2024-05-21 17:28:00 +0530

    Updated v8 testing

--- IndiaEvents.Models/obj/Debug/net8.0/IndiaEvents.Models.AssemblyInfo.cs
+++ IndiaEvents.Models/obj/Debug/net8.0/IndiaEvents.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndiaEvents.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c2daf4235f828b918ddd68a3eb8d30f1ce39e9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2626c35f40937d36efaa54a2fae3adfddc28e4d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndiaEvents.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndiaEvents.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- IndiaEventsWebApi/obj/Debug/net8.0/IndiaEventsWebApi.AssemblyInfo.cs
+++ IndiaEventsWebApi/obj/Debug/net8.0/IndiaEventsWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndiaEventsWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c2daf4235f828b918ddd68a3eb8d30f1ce39e9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2626c35f40937d36efaa54a2fae3adfddc28e4d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndiaEventsWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndiaEventsWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
