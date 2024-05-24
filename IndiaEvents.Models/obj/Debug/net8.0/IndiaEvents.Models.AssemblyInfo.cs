886c43c6b6aa032149438a8bb241a568933f0ac2
swalame123 <srikanth.n@congruentglobal.com>
2024-05-24 21:34:17 +0530

    updated program.cs

--- IndiaEvents.Models/obj/Debug/net8.0/IndiaEvents.Models.AssemblyInfo.cs
+++ IndiaEvents.Models/obj/Debug/net8.0/IndiaEvents.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndiaEvents.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+426be32964ee8397f2149de638259a4e1b89c4d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f517978587786bdbb609f6b9d773157739640d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndiaEvents.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndiaEvents.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
