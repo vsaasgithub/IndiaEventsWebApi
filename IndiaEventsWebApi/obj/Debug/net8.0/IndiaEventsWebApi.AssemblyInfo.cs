cc1e364ac9ffb69f584fb80863e1332fa9e905a1
swalame123 <srikanth.n@congruentglobal.com>
2024-06-13 19:12:18 +0530

    Updated the Webinar pre event code (replaced with sql)

--- IndiaEventsWebApi/obj/Debug/net8.0/IndiaEventsWebApi.AssemblyInfo.cs
+++ IndiaEventsWebApi/obj/Debug/net8.0/IndiaEventsWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndiaEventsWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbea81727671276481b8f73f5d406ead112f9655")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba698c726792f626022f94d2e3ef900cca05a264")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndiaEventsWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndiaEventsWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
