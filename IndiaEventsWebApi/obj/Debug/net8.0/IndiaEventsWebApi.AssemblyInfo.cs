d48d902970c440062feb9e571a12e7e3c1b60eb7
swalame123 <srikanth.n@congruentglobal.com>
2024-06-18 19:43:27 +0530

    Updated Webhook

--- IndiaEventsWebApi/obj/Debug/net8.0/IndiaEventsWebApi.AssemblyInfo.cs
+++ IndiaEventsWebApi/obj/Debug/net8.0/IndiaEventsWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndiaEventsWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f23d9e02d8d6265674703d961cb353053153bb37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab0d0e4c395e26a21e429d5ef1c313ed936b595")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndiaEventsWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndiaEventsWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
