9961b5d0fdd37016fae4d7af33c518c02bb3d95b
swalame123 <srikanth.n@congruentglobal.com>
2024-06-26 17:53:03 +0530

    updated the schedular for mis code

--- IndiaEvents.Models/obj/Debug/net8.0/IndiaEvents.Models.AssemblyInfo.cs
+++ IndiaEvents.Models/obj/Debug/net8.0/IndiaEvents.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IndiaEvents.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da0388d34c469c317b969e8bab446e5d7a953d18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c588d4f68453d34c15e27365b951742bd836dc9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("IndiaEvents.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IndiaEvents.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
